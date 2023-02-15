659816e339bc10317d7d8195ee8cbb1ada9e1933
ITkieltyk <kieltyk+gh@gmail.com>
2023-02-16 00:19:29 +0100

    Played sound history added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const app = document.getElementById("app");
+const history = document.getElementById("historyWrapper");
+let historyText = "";
 // Data for drumKit buttons
 const drumKitData = [
   {
@@ -40,21 +42,45 @@ const drumKitData = [
 ];
 // Assigning of audiofiles for each button, filename match drumKit part name, ex. snare plays snare.wav
 // Adding of click eventlisteners for each button
+// Additionally played button history for mouseclick activation added
 drumKitData.forEach((element) => {
   const drumButton = document.createElement("button");
   drumButton.id = element.keyboardKey;
   drumButton.innerHTML = `<p>${element.keyboardKey}</p><p>${element.drumPart}</p><audio src="assets/${element.drumPart}.wav"></audio>`;
   drumButton.addEventListener("click", (e) => {
+    drumButton.getElementsByTagName("audio")[0].currentTime = 0;
     drumButton.getElementsByTagName("audio")[0].play();
+    historyText === ""
+      ? (historyText = element.drumPart)
+      : (historyText = element.drumPart + " > " + historyText);
+    historyText = historyText.split(" > ");
+    historyText.length > 20 ? (historyText.length = 17) : "";
+    historyText = historyText.join(" > ");
+    history.innerText = historyText;
   });
   app.appendChild(drumButton);
 });
 
 // Success attempt to assing keybord keys to play each sound corrensponding to button id
+// Additionally played sound history for keyboard activation added
 window.addEventListener("keydown", (e) => {
   const drumAudio = document
     .getElementById(e.key)
     .getElementsByTagName("audio")[0];
+  const drumButton = document.getElementById(e.key);
   drumAudio.currentTime = 0;
   drumAudio.play();
+  historyText === ""
+    ? (historyText =
+        drumKitData[drumKitData.findIndex((el) => el.keyboardKey === e.key)]
+          .drumPart)
+    : (historyText =
+        drumKitData[drumKitData.findIndex((el) => el.keyboardKey === e.key)]
+          .drumPart +
+        " > " +
+        historyText);
+  historyText = historyText.split(" > ");
+  historyText.length > 20 ? (historyText.length = 17) : "";
+  historyText = historyText.join(" > ");
+  history.innerText = historyText;
 });
