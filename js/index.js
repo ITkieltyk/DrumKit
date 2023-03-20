8a912b310ea6967c6b705c57467d8b60e86e8e60
ITkieltyk <kieltyk+gh@gmail.com>
2023-03-21 00:47:13 +0100

    HIstory added, optimalisation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const app = document.getElementById("app");
 const history = document.getElementById("historyWrapper");
-let historyText = "";
+let historyArr = [];
 // Data for drumKit buttons
 const drumKitData = [
   {
@@ -42,7 +42,7 @@ const drumKitData = [
 ];
 // Assigning of audiofiles for each button, filename match drumKit part name, ex. snare plays snare.wav
 // Adding of click eventlisteners for each button
-// Additionally played button history for mouseclick activation added
+// Additionally played button history for click activation added
 drumKitData.forEach((element) => {
   const drumButton = document.createElement("button");
   drumButton.id = element.keyboardKey;
@@ -50,19 +50,36 @@ drumKitData.forEach((element) => {
   drumButton.addEventListener("click", (e) => {
     drumButton.getElementsByTagName("audio")[0].currentTime = 0;
     drumButton.getElementsByTagName("audio")[0].play();
-    historyText === ""
-      ? (historyText = element.drumPart)
-      : (historyText = element.drumPart + " > " + historyText);
-    historyText = historyText.split(" > ");
-    historyText.length > 20 ? (historyText.length = 17) : "";
-    historyText = historyText.join(" > ");
-    history.innerText = historyText;
+
+    historyArr.push(element.drumPart);
+
+    const newHistTile = document.createElement("div");
+    newHistTile.classList.add("historyTileStart");
+    newHistTile.innerText = historyArr[historyArr.length - 1];
+    history.appendChild(newHistTile);
+
+    setTimeout(() => {
+      newHistTile.className = "historyTile";
+      if (screen.width < 820) {
+        if (history.getElementsByTagName("div").length > 12) {
+          for (let i = 0; i < 4; i++) {
+            history.removeChild(history.firstElementChild);
+          }
+        }
+      } else {
+        if (history.getElementsByTagName("div").length > 22) {
+          for (let i = 0; i < 11; i++) {
+            history.removeChild(history.firstElementChild);
+          }
+        }
+      }
+    }, 50);
   });
   app.appendChild(drumButton);
 });
 
 // Success attempt to assing keybord keys to play each sound corrensponding to button id
-// Additionally played sound history for keyboard activation added
+// Additionally  history of played sound for keyboard activation added
 window.addEventListener("keydown", (e) => {
   const drumAudio = document
     .getElementById(e.key)
@@ -70,17 +87,30 @@ window.addEventListener("keydown", (e) => {
   const drumButton = document.getElementById(e.key);
   drumAudio.currentTime = 0;
   drumAudio.play();
-  historyText === ""
-    ? (historyText =
-        drumKitData[drumKitData.findIndex((el) => el.keyboardKey === e.key)]
-          .drumPart)
-    : (historyText =
-        drumKitData[drumKitData.findIndex((el) => el.keyboardKey === e.key)]
-          .drumPart +
-        " > " +
-        historyText);
-  historyText = historyText.split(" > ");
-  historyText.length > 20 ? (historyText.length = 17) : "";
-  historyText = historyText.join(" > ");
-  history.innerText = historyText;
+  historyArr.push(
+    drumKitData[drumKitData.findIndex((el) => el.keyboardKey === e.key)]
+      .drumPart
+  );
+
+  const newHistTile = document.createElement("div");
+  newHistTile.classList.add("historyTileStart");
+  newHistTile.innerText = historyArr[historyArr.length - 1];
+  history.appendChild(newHistTile);
+
+  setTimeout(() => {
+    newHistTile.className = "historyTile";
+    if (screen.width < 820) {
+      if (history.getElementsByTagName("div").length > 12) {
+        for (let i = 0; i < 4; i++) {
+          history.removeChild(history.firstElementChild);
+        }
+      }
+    } else {
+      if (history.getElementsByTagName("div").length > 22) {
+        for (let i = 0; i < 11; i++) {
+          history.removeChild(history.firstElementChild);
+        }
+      }
+    }
+  }, 50);
 });
